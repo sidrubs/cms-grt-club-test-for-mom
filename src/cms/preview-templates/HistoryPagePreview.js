9e0a47fd7b80b542786048b47f22df4cbeec184c
Sidney Rubidge <sidneyrubidge@gmail.com>
2021-11-30 08:30:13 -0400

    fix: remove unused variables

--- src/cms/preview-templates/HistoryPagePreview.js
+++ src/cms/preview-templates/HistoryPagePreview.js
@@ -3,14 +3,7 @@ import PropTypes from 'prop-types'
 import { HistoryPageTemplate } from '../../templates/history-page'
 
 const HistoryPagePreview = ({ entry, getAsset }) => {
-  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
-  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
 
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
-
-  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
   return (
     <HistoryPageTemplate
